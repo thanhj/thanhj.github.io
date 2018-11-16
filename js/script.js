96e244d032e088c1389c1de0f59c3c16f0fd960e
thanhj <thanh.nguyenky@singtel.com>
2018-11-16 15:03:17 +0800

    Update footer link point to latest git commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
+$(function() {
+  function GitViewModel() {
+    this.lastCommit = ko.observable();
+    this.commitTitle = ko.observable();
+    this.commitLink = ko.observable();
 
-function GitViewModel() {
-  this.lastCommit = ko.observable();
-  this.lastCommit = '2018';
-
-  var self = this;
-  $.getJSON("https://api.github.com/repos/thanhj/thanhj.github.io/commits", function(data) {
-      if (data && data.length > 0){
-        self.lastCommit = moment().format(data[0].commit.committer.date);
-      }
-  });
-}
-
-// Activates knockout.js
-ko.applyBindings(new GitViewModel());
+    var self = this;
+    $.getJSON("https://api.github.com/repos/thanhj/thanhj.github.io/commits", function(data) {
+        if (data && data.length > 0){
+          var lastCommit = data[0].commit;
+          var title = "Change: " + lastCommit.message + ' | By ' + lastCommit.committer.name;
+          self.commitTitle(title);
+          self.commitLink(lastCommit.url);
+          self.lastCommit(moment(lastCommit.committer.date).format('ll'));
+        }
+    });
+  }
+  // Activates knockout.js
+  ko.applyBindings(new GitViewModel());
+});
