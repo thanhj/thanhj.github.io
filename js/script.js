6d878fdfcaf52462bf5d6b0f3d064e6e2778bc0b
thanhj <thanh.nguyenky@singtel.com>
2018-11-16 15:07:41 +0800

    Update footer link to html page instead of api link.

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ $(function() {
     var self = this;
     $.getJSON("https://api.github.com/repos/thanhj/thanhj.github.io/commits", function(data) {
         if (data && data.length > 0){
-          var lastCommit = data[0].commit;
-          var title = "Change: " + lastCommit.message + ' | By ' + lastCommit.committer.name;
+          var latestChange = data[0];
+          var title = "Change: " + latestChange.commit.message + ' | By ' + latestChange.commit.committer.name;
           self.commitTitle(title);
-          self.commitLink(lastCommit.url);
-          self.lastCommit(moment(lastCommit.committer.date).format('ll'));
+          self.commitLink(latestChange.html_url);
+          self.lastCommit(moment(latestChange.commit.committer.date).format('ll'));
         }
     });
   }
